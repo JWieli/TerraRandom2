c8ed17afde7b3859813b4e878f7052fd819dc5fa
Julia Wielińska <j.wielinska18@gmail.com>
2024-08-04 21:53:16 +0200

    bastion

--- main.tf
+++ main.tf
@@ -19,7 +19,8 @@ locals {
   subnet        = "subnetA"
   vm            = "vm1"
   keyvault      = "xxx4"
-  nsg = "nsg1"
+  nsg           = "nsg1"
+  bastion = "bastionsubnet"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -34,7 +35,7 @@ resource "azurerm_virtual_network" "vnetA" {
   name                = local.vnet
   location            = local.location
   resource_group_name = local.resourcegroup
-  address_space       = ["10.0.0.0/23"]
+  address_space       = ["10.0.0.0/16"]
   depends_on          = [azurerm_resource_group.rg]
 }
 
@@ -42,7 +43,15 @@ resource "azurerm_subnet" "subA" {
   name                 = local.subnet
   resource_group_name  = local.resourcegroup
   virtual_network_name = local.vnet
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
+  depends_on           = [azurerm_virtual_network.vnetA]
+}
+
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = local.bastion
+  resource_group_name  = local.resourcegroup
+  virtual_network_name = local.vnet
+  address_prefixes     = ["10.0.1.0/24"]
   depends_on           = [azurerm_virtual_network.vnetA]
 }
 
@@ -58,10 +67,50 @@ resource "azurerm_network_interface" "nicA" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+resource "azurerm_network_security_group" "nic_nsg" {
+  name                = "${azurerm_network_interface.nicA.name}-nsg"
+  location            = local.location
+  resource_group_name = local.resourcegroup
+}
 
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
+resource "azurerm_network_security_rule" "allow_rdp" {
+  name                        = "Allow-RDP"
+  priority                    = 1000
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = local.resourcegroup
+  network_security_group_name = azurerm_network_security_group.nic_nsg.name
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nicA.id
+  network_security_group_id = azurerm_network_security_group.nic_nsg.id
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = "bastionhost"
+  location            = local.location
+  resource_group_name = local.resourcegroup
+  sku = "Basic"
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
+}
+
+resource "azurerm_public_ip" "bastion" {
+  name                = "public-ip"
+  location            = local.location
+  resource_group_name = local.resourcegroup
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 #kv i sekret
@@ -94,7 +143,7 @@ resource "azurerm_key_vault_access_policy" "IAMKV" {
   # object_id    = azurerm_user_assigned_identity.MID.principal_id
   # depends_on   = [azurerm_user_assigned_identity.MID]
 
-   secret_permissions = [
+  secret_permissions = [
     "Get", "List", "Delete", "Set"
   ]
 
@@ -117,8 +166,8 @@ resource "azurerm_key_vault_secret" "vmpassword" {
   name         = "vmPassword"
   value        = random_password.password.result
   key_vault_id = azurerm_key_vault.kv2.id
-  depends_on   = [
-  azurerm_key_vault.kv2,
+  depends_on = [
+    azurerm_key_vault.kv2,
   azurerm_key_vault_access_policy.IAMKV]
 }
 
@@ -126,8 +175,8 @@ resource "azurerm_key_vault_secret" "vmlogin" {
   name         = "vmlogin"
   value        = "julka123"
   key_vault_id = azurerm_key_vault.kv2.id
-  depends_on   = [
-  azurerm_key_vault.kv2,
+  depends_on = [
+    azurerm_key_vault.kv2,
   azurerm_key_vault_access_policy.IAMKV]
 }
 
@@ -158,25 +207,4 @@ resource "azurerm_windows_virtual_machine" "vm" {
     sku       = "2016-Datacenter"
     version   = "latest"
   }
-}
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = local.nsg
-  location            = local.location
-  resource_group_name = local.resourcegroup
-}
-
-resource "azurerm_network_security_rule" "allow_rdp" {
-  name                        = "Allow-RDP"
-  priority                    = 1000
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "3389"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = local.resourcegroup
-  network_security_group_name = local.nsg
-  depends_on = [azurerm_network_security_group.nsg]
 }
