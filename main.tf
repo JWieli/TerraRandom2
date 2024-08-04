aa3fe31216a630abe91b4d70e6ca6eef2c2f31b4
Julia Wielińska <j.wielinska18@gmail.com>
2024-08-04 16:15:16 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.0.0"
+      version = "3.112.0"
     }
   }
 }
@@ -18,7 +18,7 @@ locals {
   vnet          = "vnetA"
   subnet        = "subnetA"
   vm            = "vm1"
-  keyvault      = "xxx2"
+  keyvault      = "xxx4"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -34,7 +34,7 @@ resource "azurerm_virtual_network" "vnetA" {
   location            = local.location
   resource_group_name = local.resourcegroup
   address_space       = ["10.0.0.0/23"]
-  depends_on = [azurerm_resource_group.rg]
+  depends_on          = [azurerm_resource_group.rg]
 }
 
 resource "azurerm_subnet" "subA" {
@@ -42,14 +42,14 @@ resource "azurerm_subnet" "subA" {
   resource_group_name  = local.resourcegroup
   virtual_network_name = local.vnet
   address_prefixes     = ["10.0.0.0/24"]
-  depends_on = [azurerm_virtual_network.vnetA]
+  depends_on           = [azurerm_virtual_network.vnetA]
 }
 
 resource "azurerm_network_interface" "nicA" {
   name                = "NICA"
   location            = local.location
   resource_group_name = local.resourcegroup
-  depends_on = [azurerm_subnet.subA]
+  depends_on          = [azurerm_subnet.subA]
 
   ip_configuration {
     name                          = "internal"
@@ -63,7 +63,7 @@ resource "random_string" "suffix" {
   special = false
 }
 
-# #kv i sekret
+#kv i sekret
 
 data "azurerm_client_config" "current" {}
 
@@ -76,21 +76,21 @@ resource "azurerm_key_vault" "kv2" {
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
   sku_name                    = "standard"
-  depends_on = [azurerm_resource_group.rg]
+  depends_on                  = [azurerm_resource_group.rg]
 }
 
 resource "azurerm_user_assigned_identity" "MID" {
   name                = "MID"
   resource_group_name = local.resourcegroup
   location            = local.location
-  depends_on = [azurerm_resource_group.rg]
+  depends_on          = [azurerm_resource_group.rg]
 }
 
 resource "azurerm_key_vault_access_policy" "IAMKV" {
   key_vault_id = azurerm_key_vault.kv2.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = azurerm_user_assigned_identity.MID.principal_id
-  depends_on = [azurerm_user_assigned_identity.MID]
+  depends_on   = [azurerm_user_assigned_identity.MID]
 
   secret_permissions = [
     "Get", "List", "Delete", "Set"
@@ -107,14 +107,14 @@ resource "azurerm_key_vault_secret" "vmpassword" {
   name         = "vmPassword"
   value        = random_password.password.result
   key_vault_id = azurerm_key_vault.kv2.id
-  depends_on = [azurerm_key_vault.kv2]
+  depends_on   = [azurerm_key_vault.kv2]
 }
 
 resource "azurerm_key_vault_secret" "vmlogin" {
   name         = "vmlogin"
   value        = "julka123"
   key_vault_id = azurerm_key_vault.kv2.id
-  depends_on = [azurerm_key_vault.kv2]
+  depends_on   = [azurerm_key_vault.kv2]
 }
 
 resource "azurerm_windows_virtual_machine" "vm" {
